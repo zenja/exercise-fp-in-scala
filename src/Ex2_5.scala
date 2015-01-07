655684b55b907cd225fb51761cf7a1ebec09e2f6
Xing, Wang <xwang16@ebay.com>
2015-01-07 16:52:18 +0800

    ex 2.5

---
+++ src/Ex2_5.scala
@@ -0,0 +1,18 @@
+/**
+ * Implement the higher-order function that composes two functions.
+ *
+ * def compose[A,B,C](f: B => C, g: A => B): A => C
+ */
+object Ex2_5 {
+  def main(args: Array[String]): Unit = {
+    val f1: (Int => Int) = _ + 1
+    val f2: (Int => Int) = _ + 2
+    val com = compose(f1, f2)
+    println(s"10 + 1 + 2 = ${com(10)}")
+
+  }
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C = {
+    a => f(g(a))
+  }
+}
