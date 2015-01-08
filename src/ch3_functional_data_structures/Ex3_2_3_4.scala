61b97f8fec0b73e8d7b6cdcd08acf906863b5802
Xing, Wang <xwang16@ebay.com>
2015-01-08 14:05:11 +0800

    ex 3.4

--- src/ch3_functional_data_structures/Ex3_2_3_4.scala
+++ src/ch3_functional_data_structures/Ex3_2_3_4.scala
@@ -1,7 +1,7 @@
 package ch3_functional_data_structures
 
 /**
- * Ex 3.2:
+ * Ex 3.2
  * Implement the function tail for removing the first element of a List. Note that the
  * function takes constant time. What are different choices you could make in your
  * implementation if the List is Nil? We’ll return to this question in the next chapter.
@@ -9,8 +9,15 @@ package ch3_functional_data_structures
  * Ex 3.3
  * Using the same idea, implement the function setHead for replacing the first element
  * of a List with a different value.
+ *
+ * Ex 3.4
+ * Generalize tail to the function drop, which removes the first n elements from a list.
+ * Note that this function takes time proportional only to the number of elements being
+ * dropped—we don’t need to make a copy of the entire List.
+ *
+ * def drop[A](l: List[A], n: Int): List[A]
  */
-object Ex3_2_3 {
+object Ex3_2_3_4 {
   sealed trait List[+A]
 
   case object Nil extends List[Nothing]
@@ -38,6 +45,10 @@ object Ex3_2_3 {
       case Cons(x, xs) => xs
     }
 
+    def drop[A](l: List[A], n: Int): List[A] =
+      if (n <= 0) l
+      else drop(List.tail(l), n - 1)
+
     def setHead[A](l: List[A], head: A): List[A] = l match {
       case Nil => Cons(head, Nil)
       case Cons(_, xs) => Cons(head, xs)
@@ -47,15 +58,24 @@ object Ex3_2_3 {
 
   def main(args: Array[String]): Unit = {
     // Ex 3.2
+    println()
     println("Ex 3.2")
     val l = List(1,2,3,4,5)
     println(List.tail(Nil))
     println(List.tail(l))
 
     // Ex 3.3
+    println()
     println("Ex 3.3")
     println(List.setHead(Nil, 10))
     println(List.setHead(l, 999))
+
+    // Ex 3.4
+    println()
+    println("Ex 3.4")
+    println(List.drop(Nil, 0))
+    println(List.drop(Nil, 10))
+    println(List.drop(List(1,2,3,4,5), 3))
   }
 
 }
