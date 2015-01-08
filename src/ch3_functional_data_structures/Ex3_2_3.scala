5601903bd5eaddd27f1b319960f5fa94f930c685
Xing, Wang <xwang16@ebay.com>
2015-01-08 13:57:40 +0800

    ex 3.3

--- src/ch3_functional_data_structures/Ex3_2_3.scala
+++ src/ch3_functional_data_structures/Ex3_2_3.scala
@@ -1,11 +1,16 @@
 package ch3_functional_data_structures
 
 /**
+ * Ex 3.2:
  * Implement the function tail for removing the first element of a List. Note that the
  * function takes constant time. What are different choices you could make in your
  * implementation if the List is Nil? We’ll return to this question in the next chapter.
+ *
+ * Ex 3.3
+ * Using the same idea, implement the function setHead for replacing the first element
+ * of a List with a different value.
  */
-object Ex3_2 {
+object Ex3_2_3 {
   sealed trait List[+A]
 
   case object Nil extends List[Nothing]
@@ -33,12 +38,24 @@ object Ex3_2 {
       case Cons(x, xs) => xs
     }
 
+    def setHead[A](l: List[A], head: A): List[A] = l match {
+      case Nil => Cons(head, Nil)
+      case Cons(_, xs) => Cons(head, xs)
+    }
+
   }
 
   def main(args: Array[String]): Unit = {
+    // Ex 3.2
+    println("Ex 3.2")
     val l = List(1,2,3,4,5)
-    val lTail = List.tail(l)
-    println(lTail)
+    println(List.tail(Nil))
+    println(List.tail(l))
+
+    // Ex 3.3
+    println("Ex 3.3")
+    println(List.setHead(Nil, 10))
+    println(List.setHead(l, 999))
   }
 
 }
