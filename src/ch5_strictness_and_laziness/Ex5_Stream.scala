d54e316c4f1a71ae0a6dcddc1352d3f16c78251a
Xing, Wang <xwang16@ebay.com>
2015-03-10 14:12:56 +0800

    ex 5.11

--- src/ch5_strictness_and_laziness/Ex5_Stream.scala
+++ src/ch5_strictness_and_laziness/Ex5_Stream.scala
@@ -37,17 +37,27 @@ package ch5_strictness_and_laziness
  * The append method should be non-strict in its argument.
  *
  * Ex 5.8
- * Generalize ones slightly to the function constant, which returns an infinite Stream of a given value.
+ * Generalize ones slightly to the function constant,
+ * which returns an infinite Stream of a given value.
  *
  * def constant[A](a: A): Stream[A]
  *
  * Ex 5.9
- * Write a function that generates an infinite stream of integers, starting from n, then n + 1, n + 2, and so on.
+ * Write a function that generates an infinite stream of integers,
+ * starting from n, then n + 1, n + 2, and so on.
  *
  * def from(n: Int): Stream[Int]
  *
  * Ex 5.10
- * Write a function fibs that generates the infinite stream of Fibonacci numbers: 0, 1, 1, 2, 3, 5, 8, and so on.
+ * Write a function fibs that generates the infinite stream of Fibonacci numbers:
+ * 0, 1, 1, 2, 3, 5, 8, and so on.
+ *
+ * Ex 5.11
+ * Write a more general stream-building function called unfold.
+ * It takes an initial state, and a function for producing
+ * both the next state and the next value in the generated stream.
+ *
+ * def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A]
  */
 object Ex5_Stream {
   sealed trait Stream[+A] {
@@ -180,9 +190,14 @@ object Ex5_Stream {
       go(0, 1)
     }
 
-
     def from(n: Int): Stream[Int] =
       Stream.cons(n, from(n + 1))
+
+    def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] =
+      f(z) match {
+        case None => Empty
+        case Some((a, s)) => Stream.cons(a, unfold(s)(f))
+      }
   }
 
   def main(args: Array[String]): Unit = {
@@ -247,6 +262,9 @@ object Ex5_Stream {
 
     // Ex 5.10
     assert(Stream.fibs().take(8).toList == List(0, 1, 1, 2, 3, 5, 8, 13), "fibs test case 1")
+
+    // Ex 5.11
+    // check by Ex 5.12
 
     println("All tests finished.")
   }
