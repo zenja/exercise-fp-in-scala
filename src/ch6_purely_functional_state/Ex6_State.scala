e1df1248883332094107c8b3bc6f0f2097fb8552
Xing, Wang <xwang16@ebay.com>
2015-03-12 13:00:34 +0800

    ex 6.2

--- src/ch6_purely_functional_state/Ex6_State.scala
+++ src/ch6_purely_functional_state/Ex6_State.scala
@@ -7,6 +7,13 @@ package ch6_purely_functional_state
  * which doesn’t have a non-negative counterpart.
  *
  * def nonNegativeInt(rng: RNG): (Int, RNG)
+ *
+ * Ex 6.2
+ * Write a function to generate a Double between 0 and 1, not including 1. Note: You can
+ * use Int.MaxValue to obtain the maximum positive integer value, and you can use
+ * x.toDouble to convert an x: Int to a Double.
+ *
+ * def double(rng: RNG): (Double, RNG)
  */
 object Ex6_State {
   trait RNG {
@@ -27,6 +34,11 @@ object Ex6_State {
       val (i, r) = rng.nextInt
       (if (i < 0) -(i + 1) else i, r)
     }
+
+    def double(rng: RNG): (Double, RNG) = {
+      val (i, r) = nonNegativeInt(rng)
+      (i/(Int.MaxValue.toDouble + 1), r)
+    }
   }
 
   def main(args: Array[String]): Unit = {
@@ -34,8 +46,11 @@ object Ex6_State {
 
     // Ex 6.1
     assert(RNG.nonNegativeInt(rng)._1 > 0, "nonNegativeInt test case 1")
-    assert(RNG.nonNegativeInt(rng)._1 == RNG.nonNegativeInt(rng)._1, "nonNegativeInt test case 1")
+    assert(RNG.nonNegativeInt(rng)._1 == RNG.nonNegativeInt(rng)._1, "nonNegativeInt test case 2")
 
+    // Ex 6.2
+    assert(RNG.double(rng)._1 >= 0 && RNG.double(rng)._1 < 1, "double test case 1")
+    assert(RNG.double(rng)._1 == RNG.double(rng)._1, "double test case 2")
     println("All tests finished.")
   }
 }
